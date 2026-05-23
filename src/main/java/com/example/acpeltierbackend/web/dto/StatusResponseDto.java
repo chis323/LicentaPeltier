60a0c0b3a326de84bf97c44bb2db21badc1727c4
CHIS Bogdan Raul <163643594+chis323@users.noreply.github.com>
2026-05-23 17:10:16 +0300

    Clean up StatusResponseDto by removing unused fields
    Removed unused fields from StatusResponseDto class.

--- src/main/java/com/example/acpeltierbackend/web/dto/StatusResponseDto.java
+++ src/main/java/com/example/acpeltierbackend/web/dto/StatusResponseDto.java
@@ -2,19 +2,11 @@ package com.example.acpeltierbackend.web.dto;
 
 public class StatusResponseDto {
     public boolean deviceOnline;
-
     public Long ts;
-
     public Double ambientTempC;
     public Double humidityPct;
-
-    public Double hotSideTempC;
-    public Double coldSideTempC;
-
     public Integer coldFanPwm;
     public Integer hotFanPwm;
     public Boolean peltierOn;
-
     public Boolean swingOn;
-    public String fault;
 }
